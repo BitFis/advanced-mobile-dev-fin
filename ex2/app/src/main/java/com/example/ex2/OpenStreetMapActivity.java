e652f78f802486f475aec9975739cca001d3fe52
Zuercher Lucien z003019s <lucien.zuercher@siemens.com>
2019-11-20 20:32:20 +0200

    got openStreetMap running

--- ex2/app/src/main/java/com/example/ex2/OpenStreetMapActivity.java
+++ ex2/app/src/main/java/com/example/ex2/OpenStreetMapActivity.java
@@ -2,13 +2,56 @@ package com.example.ex2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.preference.PreferenceManager;
+
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.CustomZoomButtonsController;
+import org.osmdroid.views.MapView;
 
 public class OpenStreetMapActivity extends AppCompatActivity {
+    MapView map = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_open_street_map);
+
+        // fix issue with sqllite having issue writing/data to sdcard
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        Context ctx = getApplicationContext();
+        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
+
+        map = (MapView) findViewById(R.id.map);
+        map.setTileSource(TileSourceFactory.MAPNIK);
+
+        map.getZoomController().setVisibility(CustomZoomButtonsController.Visibility.ALWAYS);
+        map.setMultiTouchControls(true);
+
+        IMapController mapController = map.getController();
+        mapController.setZoom(10.5);
+        GeoPoint rovaniemi = new GeoPoint(66.5, 25.7);
+        mapController.setCenter(rovaniemi);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        map.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        map.onPause();
     }
 }
