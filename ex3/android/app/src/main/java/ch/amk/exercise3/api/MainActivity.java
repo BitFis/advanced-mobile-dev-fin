30750f1b846041af4f893537399e3f3b5ccafa97
Zurcher Lucien <lzurcher@edu.lapinamk.fi>
2019-12-05 16:13:04 +0200

    added swiping capability

--- ex3/android/app/src/main/java/ch/amk/exercise3/api/MainActivity.java
+++ ex3/android/app/src/main/java/ch/amk/exercise3/api/MainActivity.java
@@ -1,6 +1,10 @@
 package ch.amk.exercise3.api;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import ch.amk.exercise3.api.models.Feedback;
@@ -8,19 +12,33 @@ import ch.amk.exercise3.api.service.FeedbackService;
 import ch.amk.exercise3.api.ui.FeedbackItem;
 import dagger.android.AndroidInjection;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.TextView;
 
 import com.mikepenz.fastadapter.FastAdapter;
 import com.mikepenz.fastadapter.adapters.ItemAdapter;
+import com.mikepenz.fastadapter.drag.ItemTouchCallback;
+import com.mikepenz.fastadapter.drag.SimpleDragCallback;
+import com.mikepenz.fastadapter.swipe.SimpleSwipeCallback;
+import com.mikepenz.fastadapter.swipe_drag.SimpleSwipeDragCallback;
+import com.mikepenz.iconics.IconicsColor;
+import com.mikepenz.iconics.IconicsColorList;
+import com.mikepenz.iconics.IconicsDrawable;
+import com.mikepenz.iconics.IconicsSize;
+import com.mikepenz.iconics.typeface.library.materialdesigniconic.MaterialDesignIconic;
 
 import javax.inject.Inject;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ItemTouchCallback, SimpleSwipeCallback.ItemSwipeCallback {
 
     private RecyclerView recyclerView;
     private FastAdapter fastAdapter;
     private ItemAdapter itemAdapter;
+
+    //drag & drop
+    private ItemTouchHelper touchHelper;
+    private SimpleDragCallback touchCallback;
 
     @Inject
     FeedbackService feedbackService;
@@ -36,6 +54,7 @@ public class MainActivity extends AppCompatActivity {
 
         this.recyclerView = (RecyclerView) this.findViewById(R.id.feedback_list);
         this.recyclerView.setAdapter(this.fastAdapter);
+        this.recyclerView.setItemAnimator(new DefaultItemAnimator());
 
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
@@ -50,7 +69,46 @@ public class MainActivity extends AppCompatActivity {
 
         this.fastAdapter.notifyAdapterDataSetChanged();
 
+        this.fastAdapter.setOnClickListener((view, iAdapter, o, integer) -> {
+            return false;
+        });
+
+        // setup swip to delete
+        IconicsDrawable leaveBehindDrawableLeft = new IconicsDrawable(this)
+                .icon(MaterialDesignIconic.Icon.gmi_delete)
+                .color(IconicsColor.colorInt(Color.WHITE))
+                .size(IconicsSize.dp(24));
+        IconicsDrawable leaveBehindDrawableRigth = new IconicsDrawable(this)
+                .icon(MaterialDesignIconic.Icon.gmi_archive)
+                .color(IconicsColor.colorInt(Color.WHITE))
+                .size(IconicsSize.dp(24));
+
+        this.touchCallback = new SimpleSwipeDragCallback(
+                this,
+                this,
+                leaveBehindDrawableRigth,
+                ItemTouchHelper.LEFT,
+                getColor(android.R.color.holo_blue_dark)
+        )
+        .withBackgroundSwipeRight(getColor(android.R.color.holo_red_dark))
+        .withLeaveBehindSwipeRight(leaveBehindDrawableLeft);
+
+        this.touchHelper = new ItemTouchHelper(touchCallback);
+        touchHelper.attachToRecyclerView(this.recyclerView);
     }
 
+    @Override
+    public void itemTouchDropped(int i, int i1) {
 
+    }
+
+    @Override
+    public boolean itemTouchOnMove(int i, int i1) {
+        return false;
+    }
+
+    @Override
+    public void itemSwiped(int i, int i1) {
+
+    }
 }
